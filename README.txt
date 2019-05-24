221e11938543f3f04afbb2dfa877dfd9375f4590
Cameron Stevenson <cameron.stevenson03@gmail.com>
2019-05-24 13:08:24 +1200

    Can switch between CLI/GUI versions, README.txt created

---
+++ README.txt
@@ -0,0 +1,8 @@
+Dependencies: commons-io-2.6.jar
+
+Import into eclipse as an existing eclipse project, .project and .classpath are included
+and should link commons-io-2.6.jar automatically, if not add it in the java build path
+in project properties.
+
+In gameProperties.properties change "useGUI" to false for the CLI version or true for
+the GUI version of the game
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,31 @@
 package main;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Chooses which version of the program to run
  */
 public class Main {
+	
 	/**
 	 * Chooses which version of the program to run
 	 * @param args Program arguments
 	 */
 	public static void main(String[] args){
-		CLI cli = new CLI();
-		cli.run();
+		
+		
+		Properties properties = Utilities.loadPropertiesFile("gameProperties.properties");
+		boolean useGUI = Boolean.parseBoolean(properties.getProperty("useGUI"));
+		
+		if(useGUI) {
+			System.out.println("GUI not implemented");
+		}else {
+			CLI cli = new CLI();
+			cli.run();
+		}
 	}
 }
